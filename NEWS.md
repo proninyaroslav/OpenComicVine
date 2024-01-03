e81db9dff34eedf48d739f8f78e268b488ee4f4e
Yaroslav Pronin <proninyaroslav@gmail.com>
2024-01-03 12:58:52 +0300

    Version 1.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+### Version 1.1 (2024-01-03)
+
+Fix HTTP 403. ComicVine has blocked the default User Agent.
+
 ### Version 1.0 (2023-03-22)
 
 First release.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.proninyaroslav.opencomicvine"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
