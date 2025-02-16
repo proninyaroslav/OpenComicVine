1141a6e15fef11c07823fd19f03a8d4fa498b464
Bam <Mithrandir21@users.noreply.github.com>
2025-02-16 17:19:47 +0000

    Remove unnecessary import statements from multiple files

--- app/src/main/java/org/proninyaroslav/opencomicvine/ui/auth/AuthPage.kt
+++ app/src/main/java/org/proninyaroslav/opencomicvine/ui/auth/AuthPage.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
