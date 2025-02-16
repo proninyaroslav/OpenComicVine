469b027d9e3efe1fdb00a7543a6e25c40eece6de
Bam <Mithrandir21@users.noreply.github.com>
2025-02-16 13:38:09 +0000

    Update lifecycle-runtime-ktx dependency to version 2.8.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,7 +118,7 @@ dependencies {
 
     // AndroidX Core
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
 
